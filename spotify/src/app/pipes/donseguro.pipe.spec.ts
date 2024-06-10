bae24f599ecff791f00794c0f39a7be286da7799
José López <jeduardo14lopez@gmail.com>
2024-06-09 19:04:31 -0600

    Delete spotify directory
    eliminacion archivo duplicado

--- spotify/src/app/pipes/donseguro.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DonseguroPipe } from './donseguro.pipe';
-
-describe('DonseguroPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DonseguroPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
